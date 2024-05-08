5b4517d2115047421c871fee10a283084a29b8bb
Filip Jirsák <filip@jirsak.org>
2024-05-08 21:25:05 +0200

    Drobné vylepšení zadání.

--- src/components/Ukol2/index.jsx
+++ src/components/Ukol2/index.jsx
@@ -1,5 +1,5 @@
 /*
-Zadání: Vytvořte v této komponentě efekt, který se spustí při prvním zobrazení komponenty
+Zadání: Vytvořte v této komponentě efekt, který se spustí při **prvním** zobrazení komponenty
   a v konzoli vypíše text „Jsem tady“.
 */
 
