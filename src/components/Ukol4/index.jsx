941c6153ebd7d27d4250dd6c32a4505c4d67a87b
Filip Jirsák <filip@jirsak.org>
2024-05-08 21:28:21 +0200

    fix typo

--- src/components/Ukol4/index.jsx
+++ src/components/Ukol4/index.jsx
@@ -1,7 +1,7 @@
 /*
 Zadání 1: Vytvořte v této komponentě stav `nacteno`, který bude mít výchozí hodnotu `false`.
   Dále vytvořte efekt, který po 3 vteřinách od prvního načtení komponenty nastaví stav `nacteno` na `true`.
-  Představte si to jako simulaci načítání dat z API, které bude trvat zhruba 3 vteřony.
+  Představte si to jako simulaci načítání dat z API, které bude trvat zhruba 3 vteřiny.
 Zadání 2: Pomocí ternárního operátoru zobrazte text `Načítám…` nebo `Hotovo!` podle hodnoty
   ve stavu `nacteno`.
 */
