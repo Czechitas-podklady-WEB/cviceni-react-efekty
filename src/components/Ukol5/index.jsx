19c830812e40090c1d79ec7bb9dc759fa30167f4
Filip Jirsák <filip@jirsak.org>
2024-05-11 17:02:58 +0200

    Příklad možného řešení.

--- src/components/Ukol5/index.jsx
+++ src/components/Ukol5/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 /*
 Krok 1: V tomto úkolu použijeme Harry Potter API: https://hp-api.onrender.com. Dobře si prohlédněte,
@@ -12,9 +12,28 @@ Krok 3: Pokud je stav `postava` `null`, nechte zobrazen text `Načítám…`. Po
 
 export const Ukol5 = () => {
   const [postava, setPostava] = useState(null);
+  useEffect(() => {
+    const fetchCharacter = async () => {
+      const resp = await fetch(
+        "https://hp-api.onrender.com/api/character/ca3827f0-375a-4891-aaa5-f5e8a5bad225"
+      );
+      const data = await resp.json();
+      setPostava(data[0]);
+    };
+    fetchCharacter();
+  }, []);
+
   return (
     <>
-      <p>Načítám…</p>
+      {postava === null ? (
+        <p>Načítám…</p>
+      ) : (
+        <>
+          <h3>{postava.name}</h3>
+          <p>Hraje {postava.actor}</p>
+          <img src={postava.image} />
+        </>
+      )}
     </>
   );
 };
