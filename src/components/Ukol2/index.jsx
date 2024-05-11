81425f883d65030fc25bc1e863909fcf41596805
Filip Jirsák <filip@jirsak.org>
2024-05-11 13:55:28 +0200

    Poznámka k zadání.

--- src/components/Ukol2/index.jsx
+++ src/components/Ukol2/index.jsx
@@ -1,6 +1,9 @@
 /*
-Zadání: Vytvořte v této komponentě efekt, který se spustí při **prvním** zobrazení komponenty
+Zadání: Vytvořte v této komponentě efekt, který se spustí při PRVNÍM vykreslení komponenty
   a v konzoli vypíše text „Jsem tady“.
+
+Poznámka: nemáte jak ověřit změnu oproti předchozímu příkladu. Důležité je ale vzykoušet si napsat,
+  jak se píše useEffect, který se zavolá při prvním vykreslení komponenty.
 */
 
 export const Ukol2 = () => {
